452f9f099969678d1b896ed328b7af1381f1f074
Wong Chi Hong, wchtommy20013 <tommystudy20013@gmail.com>
2017-03-14 15:01:37 +0800

    rand double

---
+++ arr.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(void){
+
+	srand(time(NULL));
+
+	double *c = (double *) malloc ( 5*sizeof(double));
+	c[1] = 0.04;
+	c[2] = 0.04;
+
+	c[0] = (double) rand() / RAND_MAX + 2.8;
+	c[3] = (double) rand() / RAND_MAX + 2.8;
+
+	int i;
+	for(i = 0; i < 4; i++){
+		printf("#%d: %.5f\n", i, c[i]);
+	}
+	free(c);
+}
+
+/*
+Output: 
+#0: 1.1
+#1: 1.2
+#2: 1.3
+#3: 1.4 
+*/
